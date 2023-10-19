0bd2d192044ed3d8ca848c79d857151df247a171
compujuckel <github@mackeben.eu>
2023-10-19 20:42:16 +0200

    some changes to download section

--- src/components/home/Download.tsx
+++ src/components/home/Download.tsx
@@ -20,7 +20,7 @@ export const Download = () => {
             <div className="py-14 px-10 m-auto w-full lg:w-11/12 2xl:w-4/6">
                 <div className="flex flex-col lg:flex-row">
                     <div className="w-full lg:pr-12 lg:w-3/5">
-                        <div className="w-1/2 text-left divide-y divide-gray-500 2xl:w-1/4">
+                        <div className="w-1/2 text-left divide-y divide-blue-dark 2xl:w-1/4">
                             <h2 className="text-base font-medium tracking-widest text-blue-200 uppercase">READY TO DRIVE?</h2>
                             <p className="pt-3 mt-3 text-5xl font-medium text-blue-light">
                                 Download
@@ -40,7 +40,7 @@ export const Download = () => {
                             allowFullScreen
                         />
                     </div>
-                    <div className="flex flex-col w-full divide-y divide-blue-dark lg:w-2/5">
+                    <div className="flex flex-col w-full divide-y divide-blue-dark lg:w-2/5 justify-center">
                         <div className="pt-16 pb-8 lg:pt-0">
                             <span className="text-4xl text-blue-100">Track</span>
 
@@ -48,12 +48,12 @@ export const Download = () => {
                                 The stable release is required for most servers.
                             </p>
 
-                            <div className="divide-y divide-blue-darker">
-                                <div className="flex flex-row justify-between items-center mb-5">
+                            <div className="">
+                                <div className="flex flex-row justify-between items-center mb-2">
                                     <span className="text-xl text-gray-300">Stable Release</span>
                                     <Button onClick={() => props.setOpenModal(urls.stable)} className="float-right w-40 font-bold bg-green-500 hover:bg-green-700">Download</Button>
                                 </div>
-                                <div className="flex flex-row justify-between items-center pt-5 mb-5">
+                                <div className="flex flex-row justify-between items-center pt-5 mb-4">
                                     <span className="text-xl text-gray-300">Public Testing Build</span>
                                     <Button
                                         onClick={() => props.setOpenModal(urls.dev)}
@@ -67,13 +67,12 @@ export const Download = () => {
 
                         <div className="pt-7">
                             <span className="text-2xl text-blue-100">Car Pack</span>
-                            <p className="mt-4 mb-6 max-w-prose">
-                                Contains all cars required to play on our official servers.
-                            </p>
-
-                            <a href={urls.cars} rel="nofollow">
-                                <Button className="float-right w-40 font-bold bg-green-500 hover:bg-green-700">Download</Button>
-                            </a>
+                            <div className="flex flex-row justify-between items-center mb-5">
+                                <span className="text-xl text-gray-300">Contains all cars required to play on our official servers.</span>
+                                <a href={urls.cars} rel="nofollow">
+                                    <Button className="float-right w-40 font-bold bg-green-500 hover:bg-green-700">Download</Button>
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </div>
