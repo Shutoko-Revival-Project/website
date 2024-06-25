9a38ed0ce1e70d5d6549f2a179c978c3dcc31d73
John5i4 <34525052+John5i4@users.noreply.github.com>
2024-06-25 16:37:29 +0300

    Same thing as before but with react const.

--- src/components/home/Download.tsx
+++ src/components/home/Download.tsx
@@ -12,6 +12,12 @@ export const Download = () => {
         cars: 'https://files.shutokorevivalproject.com/SRP_Car_Pack_3.3.7z',
     };
 
+    const versions = {
+        stable: 'v0.9.1',
+        dev: '0.9.3 PTB2',
+        cars: '3.3'
+    }
+
     return (
         <section id="download" className="relative bg-blue-dark-contrast">
 
@@ -50,11 +56,11 @@ export const Download = () => {
 
                             <div className="">
                                 <div className="flex flex-row justify-between items-center mb-2">
-                                    <span className="text-xl text-gray-300">Stable Release 0.9.1</span>
+                                    <span id='stable-download-text' className="text-xl text-gray-300">Stable Release {versions.stable}</span>
                                     <Button onClick={() => props.setOpenModal(urls.stable)} className="float-right w-40 font-bold bg-green-500 hover:bg-green-700">Download</Button>
                                 </div>
                                 <div className="flex flex-row justify-between items-center pt-5 mb-4">
-                                    <span className="text-xl text-gray-300">Public Testing Build 0.9.3 PTB2</span>
+                                    <span id='ptb-download-text' className="text-xl text-gray-300">Public Testing Build (0.9.3 PTB2)</span>
                                     <Button
                                         onClick={() => props.setOpenModal(urls.dev)}
                                         className="float-right w-40 font-bold text-blue-light hover:text-blue-light hover:bg-white border-2 border-blue-light"
