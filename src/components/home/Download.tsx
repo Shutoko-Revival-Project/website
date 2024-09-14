8cff9a4a0b70f0fcfc3707e6fac5743e0ecc54d7
John5i4 <34525052+John5i4@users.noreply.github.com>
2024-09-14 16:45:05 +0300

    Alright

--- src/components/home/Download.tsx
+++ src/components/home/Download.tsx
@@ -65,18 +65,21 @@ export const Download = () => {
                                     </span>
                                     <Button onClick={() => props.setOpenModal(urls.stable)} className="float-right w-40 font-bold bg-green-500 hover:bg-green-700">Download</Button>
                                 </div>
-
                                 <div className="flex flex-row justify-between items-center pt-5 mb-4">
-                                    <span id="stable-download-text-old" className="text-xl text-gray-300">
+                                    <span id="ptb-download-text" className="text-xl text-gray-300">
                                         Old Stable Release
                                         {' '}
-                                        <span id="version-check-stable-old" style={{ display: 'inline' }}>{versions.stable_old}</span>
+                                        <span id="version-check-stable" style={{ display: 'inline' }}>{versions.stable_old}</span>
                                     </span>
-                                    <Button onClick={() => props.setOpenModal(urls.stable_old)} className="float-right w-40 font-bold text-blue-light hover:text-blue-light 
-                                    hover:bg-white border-2 border-blue-light">Download</Button>
+                                    <Button
+                                        onClick={() => props.setOpenModal(urls.stable_old)}
+                                        className="float-right w-40 font-bold text-blue-light hover:text-blue-light hover:bg-white border-2 border-blue-light"
+                                    >
+                                        Download
+                                    </Button>
                                 </div>
 
-                                {/* <div className="flex flex-row justify-between items-center pt-5 mb-4" style={{ display: 'none' }}>
+                                {/* <div className="flex flex-row justify-between items-center pt-5 mb-4">
                                     <span id="ptb-download-text" className="text-xl text-gray-300">
                                         Public Testing Build
                                         {' '}
