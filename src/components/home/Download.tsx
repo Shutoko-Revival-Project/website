4527d0e20eaaa2904bdabafb239c907618fce4bf
John5i4 <34525052+John5i4@users.noreply.github.com>
2024-09-14 16:38:57 +0300

    idk any more

--- src/components/home/Download.tsx
+++ src/components/home/Download.tsx
@@ -72,7 +72,7 @@ export const Download = () => {
                                         {' '}
                                         <span id="version-check-stable-old" style={{ display: 'inline' }}>{versions.stable_old}</span>
                                     </span>
-                                    <Button onClick={() => props.setOpenModal(urls.stable_old)} className="float-right w-40 font-bold text-blue-light hover:text-blue-light hover:bg-white">Download</Button>
+                                    <Button onClick={() => props.setOpenModal(urls.stable_old)} className="float-right w-40 font-bold hover:bg-white">Download</Button>
                                 </div>
 
                                 {/* <div className="flex flex-row justify-between items-center pt-5 mb-4" style={{ display: 'none' }}>
