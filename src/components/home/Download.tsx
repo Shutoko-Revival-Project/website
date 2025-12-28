aef4e7e031a6b2b221cf00fdd900bcf5c06f0cff
John5i4 <34525052+John5i4@users.noreply.github.com>
2025-12-28 19:36:02 +0200

    Update Download.tsx
    Car pack 3.6

--- src/components/home/Download.tsx
+++ src/components/home/Download.tsx
@@ -10,14 +10,14 @@ export const Download = () => {
         stable: 'https://files.shutokorevivalproject.com/r/SRP%200.9.3.7z',
         stable_old: 'https://files.shutokorevivalproject.com/r/SRP%20MAIN_0.9.1_OFFICIAL.7z',
         ptb: 'https://files.shutokorevivalproject.com/r/SRP%200.9.4%20PTB1.7z',
-        cars: 'https://files.shutokorevivalproject.com/SRP_Car_Pack_3.5.7z',
+        cars: 'http://files.shutokorevivalproject.com/SRP_Car_Pack_3.6.7z',
     };
 
     const versions = {
         stable: '0.9.3',
         stable_old: '0.9.1',
         ptb: '0.9.4 PTB1',
-        cars: '3.4',
+        cars: '3.6',
     };
 
     return (
